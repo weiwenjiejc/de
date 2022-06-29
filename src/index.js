685206e3153b96c78f55d9c6f5819da40074a0e0
weiwenjiejc <1941796841@qq.com>
2022-06-29 19:01:57 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 // import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom";
+import React from "react";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
