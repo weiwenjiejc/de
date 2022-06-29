1e42823a6ba97f90990d1b714dad1d46a9ef938b
weiwenjiejc <1941796841@qq.com>
2022-06-29 18:56:55 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
+// import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+// const root = createRoot(rootElement);
 
-root.render(
+ReactDOM.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
+  rootElement
 );
